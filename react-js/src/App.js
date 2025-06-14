ebb9a59d67d0fd90378b7cfc23028890e72643e2
neelima-singh07 <neelima.arun2007@gmail.com>
2025-06-14 13:03:38 +0530

    pushed customReact by hitesh sir

---
+++ react-js/src/App.js
@@ -0,0 +1,17 @@
+import Chai from "./chai"
+
+
+function App() {
+  return (
+    <div> 
+      <Chai/>
+      <h1>Welcome to React</h1>
+    </div>
+
+    
+    
+    
+  );
+}
+
+export default App;
